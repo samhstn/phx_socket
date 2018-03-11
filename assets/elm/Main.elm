7eb4ed66332cee5e0946fa8370449b73189c0743
shouston3 <shouston3@me.com>
2018-03-11 21:27:58 +0000

    working tests and pinging

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,7 +1,87 @@
 module Main exposing (..)
 
-import Html exposing (text)
-
+import Html exposing (Html, program, text, div, button)
+import Html.Events exposing (onClick)
+import Phoenix.Socket
+import Phoenix.Channel
+import Phoenix.Push
+import Json.Encode
+import Debug
 
 main =
-    text "HELLO WORLD"
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+init : ( Model, Cmd Msg )
+init =
+    let
+        channel =
+            Phoenix.Channel.init "room:lobby"
+                |> Phoenix.Channel.withPayload ( Json.Encode.object [ ( "hello", Json.Encode.string "world" ) ] )
+                |> Phoenix.Channel.onJoin (always PingServer)
+        ( phxSocket, phxCmd ) =
+            Phoenix.Socket.join channel initialModel.phxSocket
+    in
+        { initialModel | phxSocket = phxSocket } ! [ Cmd.map PhoenixMsg phxCmd ]
+
+
+initialModel : Model
+initialModel =
+    { phxSocket = Phoenix.Socket.init "ws://localhost:4000/socket/websocket"
+        |> Phoenix.Socket.withDebug
+        |> Phoenix.Socket.on "hello" "room:lobby" ReceiveHello
+    }
+
+type alias Model =
+    { phxSocket : Phoenix.Socket.Socket Msg
+    }
+
+type Msg
+    = PhoenixMsg (Phoenix.Socket.Msg Msg)
+    | PingServer
+    | ReceiveHello Json.Encode.Value
+    | NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        PhoenixMsg msg ->
+            let
+                ( phxSocket, phxCmd ) = Phoenix.Socket.update msg model.phxSocket
+            in
+               { model | phxSocket = phxSocket } ! [ Cmd.map PhoenixMsg phxCmd ]
+        PingServer ->
+            let
+                _ = Debug.log "hello" "hello"
+                payload =
+                    Json.Encode.object [ ( "pinghello", Json.Encode.string "pingworld" ) ]
+                push_ =
+                    Phoenix.Push.init "ping" "room:lobby"
+                        |> Phoenix.Push.withPayload payload
+
+                ( phxSocket, phxCmd ) = Phoenix.Socket.push push_ model.phxSocket
+            in
+               { model | phxSocket = phxSocket } ! [ Cmd.map PhoenixMsg phxCmd ]
+        ReceiveHello m ->
+            let
+                _ = Debug.log "hello" m
+            in
+                model ! []
+        NoOp ->
+            model ! []
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "hello world"
+        , button [ onClick PingServer ] [ text "ping server" ]
+        ]
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Phoenix.Socket.listen model.phxSocket PhoenixMsg
--- lib/phx_socket_web/channels/room_channel.ex
+++ lib/phx_socket_web/channels/room_channel.ex
@@ -2,24 +2,18 @@ defmodule PhxSocketWeb.RoomChannel do
   use PhxSocketWeb, :channel
 
   def join("room:lobby", payload, socket) do
+    IO.inspect payload, label: "JOIN PAYLOAD"
     if authorized?(payload) do
-      {:ok, socket}
+      {:ok, "joining", socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
   def handle_in("ping", payload, socket) do
+    IO.inspect payload, label: "HANDLE_IN PING PAYLOAD"
+    push socket, "hello", %{hello: "worlds"}
     {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
   end
 
   # Add authorization logic here as required.
